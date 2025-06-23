f9f72caacdd1b197b0b2bb559003dadd742e4788
vuWinPc <ice.hockey.greatone@gmail.com>
2025-06-23 22:26:26 +0900

    xmlのマッピングを修正

---
+++ TaskMate8/src/main/java/com/example/taskmate/TaskMate08Application.java
@@ -0,0 +1,13 @@
+package com.example.taskmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskMate08Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskMate08Application.class, args);
+	}
+
+}
